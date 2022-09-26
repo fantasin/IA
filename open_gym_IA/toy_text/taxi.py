346c9bf33efe01af8471a5d84f6ecc03ae284e5a
abodenho <anthony.bod@gmail.com>
2022-09-26 23:27:35 +0200

    taxi

---
+++ open_gym_IA/toy_text/taxi.py
@@ -0,0 +1,27 @@
+
+
+
+import gym
+from Templates.Reinforcement_Learning.template_agent_q_learning import Agent_q_learning
+from open_gym_IA.toy_text.template_game_toy_text import play_game
+
+environement_train = gym.make("Taxi-v3")
+environement_test = gym.make("Taxi-v3",render_mode="human")
+
+ALPHA = 0.1
+GAMMA = 0.999
+EPSILON = 0.80
+EPSILON_DECREASE = 0.9999
+
+list_action = [*range(environement_train.action_space.n)]
+number_state = environement_train.observation_space.n
+
+
+agent = Agent_q_learning(list_action,number_state,GAMMA,ALPHA,EPSILON,EPSILON_DECREASE)
+
+NUMBER_EPISODE = 10000
+NUMBER_TEST = 10
+
+
+play_game(environement_train,environement_test,agent,NUMBER_EPISODE,NUMBER_TEST)
+
